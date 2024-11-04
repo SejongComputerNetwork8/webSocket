e207f4552c03f0706a8ba9ae27cf95dc52b04299
Munhangyeol <mhg10181018@gmail.com>
2024-11-04 17:15:34 +0900

    Refactor: naver,gmail관련 코드 리팩토링 및 통합

--- computerNetworkAssignment/src/WebSocketApplication.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class WebSocketApplication {
-    public static void main(String[] args) throws IOException {
-        UI webSocketProgram = new UI();
-        webSocketProgram.run();
-    }
-}
--- webSocket/src/main/java/computerNetwork/webSocket/WebSocketApplication.java
+++ webSocket/src/main/java/computerNetwork/webSocket/WebSocketApplication.java
@@ -4,15 +4,14 @@ package computerNetwork.webSocket;
 
 
 
-//import computerNetwork.webSocket.gmail.GmailFetcher;
 
-import computerNetwork.webSocket.gmail.GmailMailbox;
+import computerNetwork.webSocket.gmail.GmailFetcher;
 
 import java.io.IOException;
 
 public class WebSocketApplication {
 	public static void main(String[] args) throws IOException {
-	GmailMailbox gmailFetcher=new GmailMailbox();
+	GmailFetcher gmailFetcher=new GmailFetcher();
 	gmailFetcher.checkEmails("mhg10181018@gmail.com","dkgoanypxohpizoi");
 	}
 
