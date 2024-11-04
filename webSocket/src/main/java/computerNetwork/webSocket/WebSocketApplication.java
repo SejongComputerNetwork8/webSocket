45bc53c7ae5b3d770ea6ea93d686d1cb8255747a
Munhangyeol <mhg10181018@gmail.com>
2024-11-04 18:01:51 +0900

    feat: dto

--- webSocket/src/main/java/computerNetwork/webSocket/WebSocketApplication.java
+++ webSocket/src/main/java/computerNetwork/webSocket/WebSocketApplication.java
@@ -5,12 +5,15 @@ package computerNetwork.webSocket;
 
 
 
+import computerNetwork.webSocket.dto.UserInfo;
 import computerNetwork.webSocket.gmail.GmailFetcher;
 
 import java.io.IOException;
 
 public class WebSocketApplication {
+
 	public static void main(String[] args) throws IOException {
+		UserInfo userInfo=new UserInfo("sss","sss","ssss");
 	GmailFetcher gmailFetcher=new GmailFetcher();
 	gmailFetcher.checkEmails("mhg10181018@gmail.com","dkgoanypxohpizoi");
 	}
---
+++ webSocket/src/main/java/computerNetwork/webSocket/dto/FetchingInformation.java
@@ -0,0 +1,5 @@
+package computerNetwork.webSocket.dto;
+
+public record FetchingInformation(String sendingPerson,String date,String title
+) {
+}
